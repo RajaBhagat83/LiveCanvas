e613c78f41468e51995d7df96b14441230d36ccd
Raja Bhagat <bhagatraja116@gmail.com>
2026-01-18 01:19:59 +0530

    prisma add

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,89 +1,95 @@
-import express from "express"
-import jwt  from "jsonwebtoken"
+import express, { urlencoded } from "express";
+import jwt from "jsonwebtoken";
 import { Request, Response } from "express";
-import { JWTSECRET } from "@repo/backend-comn/config"
-import {SiginSchema ,CreateUserSchema ,CreateRoomSchema} from "@repo/common/zod"
-import { prisma } from "@repo/db/prisma";
+import { JWTSECRET } from "@repo/backend-comn/config";
+import {
+  SiginSchema,
+  CreateUserSchema,
+  CreateRoomSchema,
+} from "@repo/common/zod";
+import { prisma } from "@repo/db/client";
+
+
 const app = express();
 app.use(express.json());
 
-app.get("/health",(req,res)=>{
+//health checkpoint
+app.get("/health", (req, res) => {
   return res.json({
-    msg:"Http server health is fine"
-  })
-})
+    msg: "Http server health is fine",
+  });
+});
 
-app.post('/signin',async(req,res)=>{
-  const {email ,password} =SiginSchema.parse(req.body);
-  if(!email || !password){
-    return res.json({
-      msg  : "Fill all the required Fields"
-    })
+
+//signin endpoint
+app.post("/signin", async (req, res) => {
+  const result = SiginSchema.safeParse(req.body);
+  if (!result.success) {
+    return res.status(404).json({
+      msg: "Fill all the required Fields",
+    });
   }
- //db call to find
-  const user = await prisma.User.findUnique({
-    where:{
-      email:email,
-      password:password
-    }
-  })
-  if(!user){
+  const { email, password } = result.data;
+  //db call to find
+  const user = await prisma.user.findUnique({
+    where: {
+      email: email,
+      password: password,
+    },
+  });
+  if (!user) {
     res.json("User not found Please sign up");
   }
-  const token  = jwt.sign({email : email },JWTSECRET,{ expiresIn : 3600})
+  const token = jwt.sign({ email: email }, JWTSECRET, { expiresIn: 3600 });
   return res.json({
     email,
-    token
-  })
-})
+    token,
+  });
+});
 
-app.post('/signup',async(req,res) =>{
-  const { email , password } =CreateUserSchema.parse(req.body);
-  if(!email || !password){
-    return res.json({
-      msg :"Please fill all ther required credentials"
+
+//signup endpoint
+app.post("/signup", async (req, res) => {
+  const result = CreateUserSchema.safeParse(req.body);
+  if (!result.success) {
+    return res.status(404).json({
+      msg: "Please fill all ther required credentials",
     });
   }
 
+  const { email, password } = result.data;
   //find is user already exist
-  const findUser = await prisma.User.findUnique({
-    where : {
-      email:email
-    }
-  })
-  if(findUser){
-    return res.json({
-      msg:"user already User . PLease sign in"
-    });
-  }
- //create a user and store in db
-  const createUser = prisma.User.create({
-     data:{
-      email :email,
-      password:password
-     }
+
+  //create a user and store in db
+  const createUser = await prisma.user.create({
+    data: {
+      email: email,
+      password: password,
+      name: "Raja"
+    },
   });
 
-  const token = jwt.sign({ email : email},JWTSECRET,{expiresIn:3600});
+  const token = jwt.sign({ email: email }, JWTSECRET, { expiresIn: 3600 });
   return res.json({
     email,
-    token
-  })
+    token,
+  });
+});
 
-})
 
-app.post("/createRoom",async(req,res)=>{
+//room endpoint
+app.post("/room", async (req, res) => {
   const data = CreateRoomSchema.safeParse(req.body);
-  if(!data.success){
+  if (!data.success) {
     return res.status(404).json({
-      msg:"Input has some fault data"
-    })
+      msg: "Input has some fault data",
+    });
   }
-   res.json({
-    email :"raja1@gmail.com"
-  })
-})
+  res.json({
+    email: "raja1@gmail.com",
+  });
+});
 
-app.listen(8000,() => {
-  console.log(`Serve runnning on port http://localhost:8000`);
-})
+app.listen(8001, () => {
+  console.log(`Serve runnning on port http://localhost:8001`);
+});
--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,3 +1,8 @@
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaClient } from "./generated/prisma/client.js";
+import "dotenv/config"
+import { PrismaPg } from "@prisma/adapter-pg";
 
-export const prisma  = new PrismaClient();
+const connectingString = `${process.env.DATABASE_URL}`
+
+const adapter = new PrismaPg({connectingString})
+export const prisma = new PrismaClient({adapter});
