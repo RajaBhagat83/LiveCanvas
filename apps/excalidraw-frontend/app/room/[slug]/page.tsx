e3215a589cd001b65c79dcfa1b11aed408174fbc
Raja Bhagat <bhagatraja116@gmail.com>
2026-01-27 23:55:13 +0530

    canvas starter

---
+++ apps/excalidraw-frontend/app/room/[slug]/page.tsx
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { BACKEND_URL } from "../../config";
+import Chats from "../../../Components/Chats";
+
+async function getRoomId(slug:string){
+   const response = await axios(`${BACKEND_URL}/room/${slug}`);
+   return response.data.room.id;
+}
+
+export default async function Chatroom({
+  params
+}:{
+  params:{
+    slug:string 
+  }
+}) {
+  const { slug } =await params;
+  const roomId = await getRoomId(slug);
+  return <Chats id={roomId}  />
+
+}
